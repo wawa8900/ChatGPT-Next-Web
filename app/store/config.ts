996387064f3a35370ebb0d7153f34b839ddac89b
liufeng <liufeng@jugui-tech.com>
2023-08-07 22:15:57 +0800

    隐藏自定义模型

--- app/store/config.ts
+++ app/store/config.ts
@@ -163,7 +163,7 @@ export const useAppConfig = create<ChatConfigStore>()(
         }
 
         if (version < 3.5) {
-          // state.customModels = "claude,claude-100k";
+          state.customModels = "";
         }
 
         if (version < 3.6) {
